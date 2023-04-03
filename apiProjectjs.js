c30bed578ddcd8fd21104d597dff0eb7bbb4719c
Courtney Sherman <csherman177@gmail.com>
2023-04-02 19:18:39 -0600

    new code with the search results saving

--- apiProjectjs.js
+++ apiProjectjs.js
@@ -1,10 +1,7 @@
 var searchBtn = document.querySelector("#search-btn"); // Search button
 var searchInput = document.querySelector("#city-input"); // Input for Search
 var weatherResultsEl = document.querySelector("#first");
-var weatherData = [];
-var parentEl = document.querySelector("#parent"); // Parent element for search results
-var resultsContainerEl = document.createElement("div"); // Container for search results
-parentEl.appendChild(resultsContainerEl); // Append container to parent element
+var searchedCities= [];
 
 var API_KEY = "33bf6212a02f55406c312de7ffdcf207";
 var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?appid=" + API_KEY;
@@ -22,10 +19,7 @@ function weather() {
       if (data.list) {
         var forecast = data.list;
 
-        // clear previous results
-        //weatherResultsEl.innerHTML = "";
-
-        // get the weather data for every day for the next 5 days
+          // get the weather data for every day for the next 5 days
         var dailyWeather = [];
 
         // get the weather data for every day for the next 5 days
@@ -49,22 +43,22 @@ function weather() {
           var tempEl = document.createElement("p");
           var windEl = document.createElement("p");
           var humidityEl = document.createElement("p");
+          //var iconEl = document.createElement("i";) //create icon element
 
           // add data to the elements
           dateEl.textContent = day.dt_txt.split(" ")[0];
           tempEl.textContent = "Temperature: " + day.main.temp;
           windEl.textContent = "Wind: " + day.wind.speed;
           humidityEl.textContent = "Humidity: " + day.main.humidity;
+          //iconEl.classList.add("fa-solid fa-cloud"); 
 
           // append the elements to the day's weather div
-          dayEl.append(dateEl, tempEl, windEl, humidityEl);
+          dayEl.append(dateEl, tempEl, windEl, humidityEl,);          
 
-           // append the div to the results container
-           resultsContainerEl.appendChild(dayEl);
-
-          // append the div to the results container
+         // append the div to the results container
           weatherResultsEl.appendChild(dayEl);
         });
+
       } else {
         console.error("No weather data found");
       }
@@ -72,6 +66,26 @@ function weather() {
     .catch(function (error) {
       console.error("Error fetching weather data", error);
     });
+
+    // check if the searched city is already in the array
+    if (!searchedCities.includes(city)) {
+    // add the searched city to the array
+    searchedCities.push(city);
+
+    // create a new button element for the searched city
+    var cityBtnEl = document.createElement("button");
+    cityBtnEl.textContent = city;
+
+    // add an event listener to the button to search for the city again
+    cityBtnEl.addEventListener("click", function () {
+      searchInput.value = city;
+      weather();
+  });
+
+  // append the button to the HTML element
+  var searchedCitiesListEl = document.querySelector("#searched-cities-list");
+  
+  searchedCitiesListEl.appendChild(cityBtnEl);
 }
 
 searchBtn.addEventListener("click", function () {
